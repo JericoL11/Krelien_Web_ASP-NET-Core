efd6f26cba7f4c38993c739977870959b1ba621d
JericoL <jericolabajo11@gmail.com>
2023-10-15 10:24:39 +0800

    Login Page Done

---
+++ Labajo_WebApp/Pages/LoginRegister/LoginPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Labajo_WebApp.Pages.LoginRegister
+{
+    public class LoginPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
